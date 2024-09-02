e56a797ddeff74b71e246d00432f63c4be38accb
M-elhamdaoui <melhamdaoui811@gmail.com>
2024-09-02 19:00:56 +0100

    fix some problems in movement between 2 walls

--- moves/move_f_b.c
+++ moves/move_f_b.c
@@ -6,7 +6,7 @@
 /*   By: mel-hamd <mel-hamd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 16:41:12 by mel-hamd          #+#    #+#             */
-/*   Updated: 2024/09/02 18:42:03 by mel-hamd         ###   ########.fr       */
+/*   Updated: 2024/09/02 18:58:38 by mel-hamd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,19 @@ void	move_f_b(t_cub3d *c)
 	double	ang;
 	double	tmp;
 	double	tmp2;
+	double	r;
 
+	r = c->p.rad;
 	ang = c->p.rot_ang;
 	tmp = c->p.c.x + c->p.ms * cos(ang) * c->p.wd;
 	tmp2 = c->p.c.y + c->p.ms * sin(ang) * c->p.wd;
-	if (c->map.map[(int)(tmp2 / US)][(int)(tmp / US)] == '1')
+	if (c->map.map[(int)((tmp2) / US)][(int)((tmp + r) / US)] == '1')
+		return ;
+	else if (c->map.map[(int)((tmp2) / US)][(int)((tmp - r) / US)] == '1')
+		return ;
+	else if (c->map.map[(int)((tmp2 + r) / US)][(int)((tmp) / US)] == '1')
+		return ;
+	else if (c->map.map[(int)((tmp2 - r) / US)][(int)((tmp) / US)] == '1')
 		return ;
 	c->p.c.y = tmp2;
 	c->p.c.x = tmp;
--- moves/move_l_r.c
+++ moves/move_l_r.c
@@ -6,7 +6,7 @@
 /*   By: mel-hamd <mel-hamd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 17:51:22 by mel-hamd          #+#    #+#             */
-/*   Updated: 2024/09/02 18:39:45 by mel-hamd         ###   ########.fr       */
+/*   Updated: 2024/09/02 18:58:08 by mel-hamd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,21 @@ void	move_l_r(t_cub3d *c)
 	double	ang;
 	double	tmp;
 	double	tmp2;
+	double	r;
 
+	r = c->p.rad;
 	if (c->p.wd_h == 0)
 		return ;
 	ang = c->p.rot_ang + (M_PI / 2) * c->p.wd_h;
 	tmp = c->p.c.x + c->p.ms * cos(ang);
 	tmp2 =c->p.c.y + c->p.ms * sin(ang);
-	if (c->map.map[(int)(tmp2 / US)][(int)(tmp / US)] == '1')
+	if (c->map.map[(int)((tmp2) / US)][(int)((tmp + r) / US)] == '1')
+		return ;
+	else if (c->map.map[(int)((tmp2) / US)][(int)((tmp - r) / US)] == '1')
+		return ;
+	else if (c->map.map[(int)((tmp2 + r) / US)][(int)((tmp) / US)] == '1')
+		return ;
+	else if (c->map.map[(int)((tmp2 - r) / US)][(int)((tmp) / US)] == '1')
 		return ;
 	c->p.c.y = tmp2;
 	c->p.c.x = tmp;
