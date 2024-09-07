ab88d6c416cc6926b904282202535b1795e3e408
M-elhamdaoui <melhamdaoui811@gmail.com>
2024-09-07 19:24:31 +0100

    fix segv in cast_rays_h.c

--- raycasting/cast_rays_v.c
+++ raycasting/cast_rays_v.c
@@ -6,7 +6,7 @@
 /*   By: mel-hamd <mel-hamd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/06 17:41:49 by mel-hamd          #+#    #+#             */
-/*   Updated: 2024/09/07 17:45:36 by mel-hamd         ###   ########.fr       */
+/*   Updated: 2024/09/07 19:22:29 by mel-hamd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ t_ray	cast_ray_v(double ang, int is_down, int is_left, t_cub3d *c)
 			tmp = intercept.x - 1;
 		else
 			tmp = intercept.x + 1;
-		if (floor(tmp / US) < c->map.width && c->map.map[(int)floor(intercept.y / US)][(int)floor(tmp / US)] == '1')
+		if (floor(tmp / US) < c->map.width && (int)floor(intercept.y / US) < c->map.height && (int)floor(intercept.y / US) >= 0 && floor(tmp / US) >= 0 && c->map.map[(int)floor(intercept.y / US)][(int)floor(tmp / US)] == '1')
 		{
 			ray.distance = calc_distance(intercept, c->p.c);
 			return (ray.is_w_hited = 1, ray.hit_wall = intercept, ray);
